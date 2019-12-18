e541c2e004568e4a3afde5862f57eb27694e3811
MariusMJ <marius.m.j@hotmail.com>
2019-12-18 13:19:32 +0100

    Dynamic welcome text

--- vy-android-app/app/src/main/java/com/lemon/vy3000/ui/search/SearchFragment.java
+++ vy-android-app/app/src/main/java/com/lemon/vy3000/ui/search/SearchFragment.java
@@ -1,24 +1,49 @@
 package com.lemon.vy3000.ui.search;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.lemon.vy3000.R;
 
+import org.w3c.dom.Text;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class SearchFragment extends Fragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_search, container, false);
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+        ((TextView) root.findViewById(R.id.textView)).setText(getWelcomeText());
 
         return root;
+    }
+
+    private String getWelcomeText() {
+        String text;
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH");
+        LocalDateTime currentHour = LocalDateTime.now();
+        int currentHourFormatted = Integer.parseInt(dtf.format(currentHour));
+
+        text = "";
+        
+        if (currentHourFormatted >= 4 && currentHourFormatted < 12) text = "morgen";
+        else if (currentHourFormatted >= 12 && currentHourFormatted < 15) text = "dag";
+        else if (currentHourFormatted >= 15 && currentHourFormatted < 18) text = "ettermiddag";
+        else if (currentHourFormatted >= 18 && currentHourFormatted < 24) text = "kveld";
+
+        return "God " + text;
     }
 }
